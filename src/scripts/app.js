c2f62370545ecda14579ab37797b4332b0a19cfc
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-02-02 20:39:39 +0000

    playerName and center, more css

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,8 @@
-console.log('%c D2 Synergy', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+console.log('%c D2 SYNERGY _V0.3', 'font-weight: bold;font-size: 40px;color: white;');
 var log = console.log.bind(console);
 var localStorage = window.localStorage;
 
-const AxiosConfig = {
-    headers: {
-        "Authorization": `Basic ${btoa('38074:9qBsYpKC7ieWB4pffobacY7weIcziSmmfDXc.nwe8S8')}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-};
+
 const GenerateState = (len) => {
     let result = ' ';
     let characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
--- src/scripts/user.js
+++ src/scripts/user.js
@@ -1,6 +1,7 @@
-console.log('%c D2 Synergy', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+console.log('%cD2 SYNERGY _V0.3', 'font-weight: bold;font-size: 40px;color: white;');
 var log = console.log.bind(console);
 var localStorage = window.localStorage;
+
 
 const AuthorizeBungie = async () => {
     var AuthorizationCode = window.location.search.replace('?code=','');
@@ -11,10 +12,7 @@ const AuthorizeBungie = async () => {
             "Content-Type": "application/x-www-form-urlencoded",
         }
     };
-
-    log('Fetching components from the Bungie.net API..')
-    document.getElementsByClassName('loading-bar--inactive')[0]['classList']['value'] = 'loading-bar loading-bar--active';
-
+    
     try {
         await axios.post('https://www.bungie.net/platform/app/oauth/token/', `grant_type=authorization_code&code=${AuthorizationCode}`, AxiosConfig)
         .then(res => {
@@ -54,33 +52,28 @@ const FetchBungieUserDetails = async () => {
     bungieNetUser['DestinyUserComponents'] = GetProfileComponents.data['Response'];
     localStorage.setItem('bungieNetUser', JSON.stringify(bungieNetUser));
 
-    log('API Fetch Complete!')
-    document.getElementsByClassName('loading-bar--active')[0]['classList']['value'] = 'loading-bar loading-bar--inactive';
+    log('API Fetch Complete!');
 };
 
 
 const ParseUserCharacters = async () => {
-    var components = JSON.parse(localStorage.getItem('components'));
     var bungieNetUser = JSON.parse(localStorage.getItem('bungieNetUser'));
-    var AxiosConfig = {
-        headers: {
-            "X-API-Key": 'e62a8257ba2747d4b8450e7ad469785d',
-            Authorization: `Bearer ${components['access_token']}`
-        }
-    };
-
     var CharacterIndex = Object.values(bungieNetUser['DestinyUserComponents']['characters']['data']);
 
+    document.getElementById('displayName').innerHTML = bungieNetUser['BungieNetUser']['uniqueName'];
+
     if (CharacterIndex[0]['emblemBackgroundPath']) {
-        document.getElementById('emblemPath1').display = 'inline-block';
+        document.getElementById('userEmblem1').style.display = 'inline';
         document.getElementById('emblemPath1').src = `https://www.bungie.net${CharacterIndex[0]['emblemBackgroundPath']}`;
     };
     if (CharacterIndex[1]['emblemBackgroundPath']) {
-        document.getElementById('emblemPath2').display = 'inline-block';
+        document.getElementById('userEmblem2').style.display = 'inline';
+        document.getElementById('emblemPath2').style.display = 'inline';
         document.getElementById('emblemPath2').src = `https://www.bungie.net${CharacterIndex[1]['emblemBackgroundPath']}`;
     };
     if (CharacterIndex[2]['emblemBackgroundPath']) {
-        document.getElementById('emblemPath3').display = 'inline-block';
+        document.getElementById('userEmblem3').style.display = 'inline';
+        document.getElementById('emblemPath3').style.display = 'inline';
         document.getElementById('emblemPath3').src = `https://www.bungie.net${CharacterIndex[2]['emblemBackgroundPath']}`;
     };
 };
@@ -88,6 +81,7 @@ const ParseUserCharacters = async () => {
 
 const Main = async () => {
     if (window.location.search.replace('?code=','')) {
+
         // Authorization Process
         await AuthorizeBungie();
         await FetchBungieUserDetails();
@@ -97,7 +91,7 @@ const Main = async () => {
     } 
     else {
         // -- Do 404.html or make an alternate authorization path -- //
-        
+
         // log('Authorization Failed, Redirecting back to root..')
         // window.location.href = 'http://localhost:4645/D2Synergy-v3.0/src/views/app.html';
     };
