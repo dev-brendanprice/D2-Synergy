35bbdf4e3392f6f8436dc871e645f357caa85d8b
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-02-18 18:41:41 +0000

    indexeddb

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -8,6 +8,7 @@ var log = console.log.bind(console),
     GetMembershipsById = {},
     UserProfile = {};
 
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 
 // Default axios header
 axios.defaults.headers.common = {
@@ -34,67 +35,73 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-var ConfigIndexedDB = async () => {
+// var db = new Dexie("ManifestDefinitions");
 
-    let openRequest = indexedDB.open('Definitions', 1);
+// // DB with single table 'entries'
+// db.version(1).stores({
+//   entries: `keyName`
+// });
 
-    openRequest.onupgradeneeded = (event) => {
-        // the existing database version is less than 2 (or it doesn't exist)
-        if (event.oldVersion == 0) {
-            // client did not have a database
-            // initialize one
-        } else if (event.oldVersion != 1) {
-            // client has an outdated version
-            // update it
-        };
-    };
-};
+// // Adding values to the table 'entries'
+// db.entries.bulkPut([
+//   { keyName: 'DestinyMobileGlobalDefinitions', name: "Josephine", age: 21 },
+//   { keyName: 'urmum', name: "lmfao", age: 69420 }
+// ]);
+
+
+// GET VALUES
+// db.table('entries').toArray()
+//     .then(res => {
+//         log(res);
+//     });
+
+// CHANGE VALUES
+// db.entries.put({keyName: 'urmum', name: 'cum'});
+
 
 
 var AuthorizeBungie = async () => {
 
-    var AuthorizationCode = window.location.search.replace('?code=','');
+    var AuthorizationCode = window.location.search.replace('?code=', '');
     try {
         if (AuthorizationCode && !localStorage.getItem('components')) {
 
-            var AccessToken = {}, 
-            RefreshToken = {},
-            components = {};
+            var AccessToken = {}, RefreshToken = {}, components = {};
             const AuthorizationConfig = {
                 headers: {
                     Authorization: `Basic ${btoa('38074:9qBsYpKC7ieWB4pffobacY7weIcziSmmfDXc.nwe8S8')}`,
                     "Content-Type": "application/x-www-form-urlencoded",
                 }
             };
-    
+
             await axios.post('https://www.bungie.net/platform/app/oauth/token/', `grant_type=authorization_code&code=${AuthorizationCode}`, AuthorizationConfig)
-            .then(res => {
-                var data = res.data;
-    
-                components['membership_id'] = data['membership_id'];
-                components['token_type'] = data['token_type'];
-                components['authorization_code'] = AuthorizationCode;
-    
-                AccessToken['inception'] = Math.round(new Date().getTime()/1000);
-                AccessToken['expires_in'] = data['expires_in'];
-                AccessToken['value'] = data['access_token'];
-    
-                RefreshToken['inception'] = Math.round(new Date().getTime()/1000);
-                RefreshToken['expires_in'] = data['refresh_expires_in'];
-                RefreshToken['value'] = data['refresh_token'];
-    
-                localStorage.setItem('accessToken', JSON.stringify(AccessToken));
-                localStorage.setItem('components', JSON.stringify(components));
-                localStorage.setItem('refreshToken', JSON.stringify(RefreshToken));
-            });
+                .then(res => {
+                    var data = res.data;
+
+                    components['membership_id'] = data['membership_id'];
+                    components['token_type'] = data['token_type'];
+                    components['authorization_code'] = AuthorizationCode;
+
+                    AccessToken['inception'] = Math.round(new Date().getTime() / 1000);
+                    AccessToken['expires_in'] = data['expires_in'];
+                    AccessToken['value'] = data['access_token'];
+
+                    RefreshToken['inception'] = Math.round(new Date().getTime() / 1000);
+                    RefreshToken['expires_in'] = data['refresh_expires_in'];
+                    RefreshToken['value'] = data['refresh_token'];
+
+                    localStorage.setItem('accessToken', JSON.stringify(AccessToken));
+                    localStorage.setItem('components', JSON.stringify(components));
+                    localStorage.setItem('refreshToken', JSON.stringify(RefreshToken));
+                });
 
             log('-> Authorized with Bungie.net!');
         }
         else if (!AuthorizationCode || !localStorage.getItem('components')) {
             window.location.href = `http://86.2.10.33:4645/D2Synergy-v3.0/src/views/app.html`;
         };
-    } 
-    catch (error) { 
+    }
+    catch (error) {
         window.location.href = `http://86.2.10.33:4645/D2Synergy-v3.0/src/views/app.html`;
     };
 
@@ -102,16 +109,12 @@ var AuthorizeBungie = async () => {
     // Check if user entered the URL in-directly
     // Check against userAuth localStorage variable to see if user has authorized before
     //  and is entering the path directly
-
-
     // try {
     //     if (AuthorizationCode && localStorage.getItem('components')) {
-
     //         // Refresh tokens completley
     //         var accessTokenKey = JSON.parse(localStorage.getItem('accessToken'));
     //         var components = JSON.parse(localStorage.getItem('components'));
     //         var refreshTokenKey = JSON.parse(localStorage.getItem('refreshToken'));
-    
     //         if (accessTokenKey && refreshTokenKey && components) {
     //             if (Math.round(new Date().getTime()/1000) - accessTokenKey['inception'] == accessTokenKey['expires_in']) {
     //                 window.location.href = `https://www.bungie.net/en/oauth/authorize?&client_id=38074&response_type=code`;
@@ -134,25 +137,57 @@ var AuthorizeBungie = async () => {
 };
 
 
+var UpdateIndexedDB = async (obj) => {
+
+    // Create database
+    var db = new Dexie("ManifestDefinitions");
+
+    // Configure the key
+    db.version(1).stores({
+        entries: `keyName`
+    });
+
+    // Put objects into table
+    db.entries.bulkPut([
+        // { keyName: }
+    ]);
+    log('bruh')
+};
 var GetDestinyManifest = async () => {
 
-    var manifestVersion,
+    // Create database
+    var db = new Dexie("ManifestDefinitions");
+
+    // Configure the key
+    db.version(1).stores({
+        entries: `keyName`
+    });
+
+    var manifestVersion = localStorage.getItem('destinyManifestVersion'),
         sTime = new Date(),
-        manifestVersion = localStorage.getItem('destinyManifestVersion');
+        ManifestContent = {};
 
-    if (manifestVersion) {
-        var CurrentManifest = await axios.get(`https://www.bungie.net/Platform/Destiny2/Manifest/`);
-        if (CurrentManifest.data.Response.version != manifestVersion) {
-            log('-> New Manifest Version Available..');
-            
-            // request new manifest to be downloaded and update version
-            // save CurrentManifest to indexddb thing idfk
-            log(CurrentManifest.data.Response.jsonWorldComponentContentPaths.en)
+    var resManifest = await axios.get(`https://www.bungie.net/Platform/Destiny2/Manifest/`);
+    if (resManifest.data.Response.version != manifestVersion) {
+        log('-> New Manifest Version Available..');
 
-            localStorage.setItem('destinyManifestVersion', CurrentManifest.data.Response.version);
-            log(`-> Manifest Version Updated! [${new Date() - sTime}ms]`);
+        ManifestContent = resManifest.data.Response;
+        // arrToCast = {};
+        for (property in ManifestContent) {
+            // arrToCast[property] = ManifestContent[property];
+            // Put objects into table
+            db.entries.bulkPut([
+                { keyName: property, data: ManifestContent[property] }
+            ]);
         };
+        // log(arrToCast)
+        // await UpdateIndexedDB(arrToCast);
+
+        localStorage.setItem('destinyManifestVersion', resManifest.data.Response.version);
     };
+
+    log(`-> Manifest Upto Date! [${new Date() - sTime}ms]`);
+
 };
 
 
@@ -226,6 +261,43 @@ var LoadCharacter = async (classType) => {
 };
 
 
+// -- [Modules] -- 
+
+// Returns corresponding class name using classType
+// @int {classType}
+var parseChar = (classType) => {
+    var r;
+    if (classType === 0) {
+        r='Titan'
+    }else if (classType === 1) {
+        r='Hunter'
+    }else if (classType === 2) {
+        r='Warlock'
+    }else{ console.error('could not parse character, parseChar() @function'); };
+    return r;
+};
+// Returns size of data that currently resides in localStorage
+// @window.obj {localStorage}
+var GetLsSize = async (localStorage) => {
+    var values = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+
+    while (i--) { values.push(localStorage.getItem(keys[i])); };
+
+    log('[Usage Bytes]: ', encodeURI(JSON.stringify(values)).split(/%..|./).length - 1);
+};
+// Query item against manifest via an itemHash
+// @int {itemHash}
+var QueryItemHash = async (itemHash) => {
+    var res = await axios.get(`https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/${itemHash}/`, { headers: { Authorization: `Bearer ${JSON.parse(localStorage.getItem('accessToken')).value}` }});
+    return res;
+};
+
+
+
+
+
 // Main
 (async () => {
 
@@ -244,7 +316,5 @@ var LoadCharacter = async (classType) => {
         isPageLoaded === true ? document.getElementById('slider').style.display = 'none' : null;
     });
 
-    log('-> OAuth Flow Done!');
-
-    log(`-> [${(new Date() - startTime)}ms]`);
+    log(`-> OAuth Flow Done! [${(new Date() - startTime)}ms]`);
 })();
