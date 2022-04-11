a6984eb4bd87579ba3dca5533fa4701e232eaf36
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-04-11 20:17:46 +0100

    bountyConfigs

--- www/assets/scripts/utils/BuildStruct.js
+++ www/assets/scripts/utils/BuildStruct.js
@@ -7,19 +7,19 @@ var log = console.log.bind(),
         DamageTypeHash: [],
         ItemCategoryHash: [],
         KillTypeHash: []
-    },
-    sTime = new Date();
+    };
 
 var definitions = await axios.get('https://www.bungie.net/common/destiny2_content/json/en/DestinyInventoryItemDefinition-cb4bec6f-e2b6-4f44-8593-cfd0255b89f2.json'),
     inventoryItemDefinitions = definitions.data.Response;
 
-for (var entry in inventoryItemDefinitions) {
-    // log(definitions[entry]);
+
+Object.keys(inventoryItemDefinitions).forEach(entry => {
     var rt = inventoryItemDefinitions[entry];
     if (rt.itemType===26) {
         bountyConfigs[rt.itemHash] = struct;
     };
-};
+});
 
+// cout
+log(inventoryItemDefinitions);
 log(bountyConfigs);
-log(`~ [${new Date() - sTime} ms]`);
