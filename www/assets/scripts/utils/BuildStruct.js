8f524cd336e072bde6c5387906e18273e67e7690
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-04-11 20:30:42 +0100

    bountyConfigs

--- www/assets/scripts/utils/BuildStruct.js
+++ www/assets/scripts/utils/BuildStruct.js
@@ -16,6 +16,7 @@ var definitions = await axios.get('https://www.bungie.net/common/destiny2_conten
 
 const build = async () => {
     Object.keys(inventoryItemDefinitions).forEach(entry => {
+        log(entry);
         if (entry.itemType===26) {
             confs[entry.itemHash] = struct;
         };
