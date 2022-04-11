9eec98e97bafb8ef797ae1a19e64234741fe03eb
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-04-11 19:43:36 +0100

    bountyConfigs

--- www/assets/scripts/utils/BuildStruct.js
+++ www/assets/scripts/utils/BuildStruct.js
@@ -2,4 +2,6 @@ import { bountyConfigs } from './SynergyDefinitions.js';
 
 const log = console.log.bind();
 
-log(bountyConfigs);
+var fubar = await axios.get('https://bungie.net/common/destiny2_content/json/en/DestinyInventoryItemDefinition-cb4bec6f-e2b6-4f44-8593-cfd0255b89f2.json');
+
+log(fubar);
