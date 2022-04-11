3542a29bef442cfeeb989bcd02287b825b5f86d4
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-04-11 22:36:24 +0100

    bountyConfigs

--- www/assets/scripts/utils/BuildStruct.js
+++ www/assets/scripts/utils/BuildStruct.js
@@ -8,22 +8,27 @@ var log = console.log.bind(),
         ItemCategory: [],
         KillType: []
     },
-    confs = {};
+    confs = {},
+    bountiesOnVendor = [];
 
 var definitionsForItems = await axios.get('https://www.bungie.net/common/destiny2_content/json/en/DestinyInventoryItemDefinition-cb4bec6f-e2b6-4f44-8593-cfd0255b89f2.json'),
     inventoryItemDefinitions = definitionsForItems.data;
 
-Object.keys(inventoryItemDefinitions).forEach(entry => {
-    if (inventoryItemDefinitions[entry].itemType===26) {
-        confs[entry] = struct;
-    };
-});
+// Object.keys(inventoryItemDefinitions).forEach(entry => {
+//     if (inventoryItemDefinitions[entry].itemType===26) {
+//         confs[entry] = struct;
+//     };
+// });
 
 var definitionsForVendors = await axios.get('https://www.bungie.net/common/destiny2_content/json/en/DestinyVendorDefinition-cb4bec6f-e2b6-4f44-8593-cfd0255b89f2.json'),
     vendorDefinitions = definitionsForVendors.data;
 
-log(definitionsForVendors);
+var zavala = definitionsForVendors.data[69482069];
+
+for (var item of zavala.itemList) {
+    inventoryItemDefinitions[item.itemHash].itemType===26 ? bountiesOnVendor.push(item) : null;
+};
 
 
 // document.getElementById('fubar').innerHTML = `${JSON.stringify(confs)}`;
-// log(confs);
+log(bountiesOnVendor);
