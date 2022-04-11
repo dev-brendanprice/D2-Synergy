3fa475a54be018985e4fd4e56cdef49be9f73ca5
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-04-11 09:48:13 +0100

    Objective Hashes Dev

--- www/assets/scripts/user.js
+++ www/assets/scripts/user.js
@@ -360,7 +360,7 @@ var LoadCharacter = async (classType) => {
     // Assign objectives to each item
     objectiveDefinitions = await ReturnEntry('DestinyObjectiveDefinition');
     Object.keys(objectiveDefinitions).forEach(v => {
-        log(v);
+        log(objectiveDefinitions[v].uiStyle);
     });
     // Object.keys(charBounties).forEach(v => {
     //     var objective = objectiveDefinitions[charBounties[v].objectives.objectiveHashes[0]];
