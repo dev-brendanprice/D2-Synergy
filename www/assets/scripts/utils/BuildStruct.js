a2f9d76616488211426822f499dbda41c27de837
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-04-11 19:49:17 +0100

    bountyConfigs

--- www/assets/scripts/utils/BuildStruct.js
+++ www/assets/scripts/utils/BuildStruct.js
@@ -2,6 +2,11 @@ import { bountyConfigs } from './SynergyDefinitions.js';
 
 const log = console.log.bind();
 
-var fubar = await axios.get('https://bungie.net/common/destiny2_content/json/en/DestinyInventoryItemDefinition-cb4bec6f-e2b6-4f44-8593-cfd0255b89f2.json', { headers: { Authorization: `Bearer ${JSON.parse(localStorage.getItem('accessToken')).value}`, "X-API-Key": "e62a8257ba2747d4b8450e7ad469785d" }});
+// Add default headers back, in case OAuthFlow needed a refresh
+axios.defaults.headers.common = {
+    "X-API-Key": "e62a8257ba2747d4b8450e7ad469785d"
+};
+
+var fubar = await axios.get('https://www.bungie.net/common/destiny2_content/json/en/DestinyInventoryItemDefinition-cb4bec6f-e2b6-4f44-8593-cfd0255b89f2.json');
 
 log(fubar);
