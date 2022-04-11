90adf0056a93f4aeeee42b37602e2575e8cb36c8
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-04-11 20:05:38 +0100

    bountyConfigs

--- www/assets/scripts/utils/BuildStruct.js
+++ www/assets/scripts/utils/BuildStruct.js
@@ -1,7 +1,20 @@
 import { bountyConfigs } from './SynergyDefinitions.js';
 
-const log = console.log.bind();
+var log = console.log.bind(),
+    struct = {
+        DestinationHash: [],
+        ActivityModeHash: [],
+        DamageTypeHash: [],
+        ItemCategoryHash: [],
+        KillTypeHash: []
+    };
 
-var fubar = await axios.get('https://www.bungie.net/common/destiny2_content/json/en/DestinyInventoryItemDefinition-cb4bec6f-e2b6-4f44-8593-cfd0255b89f2.json');
+var definitions = await axios.get('https://www.bungie.net/common/destiny2_content/json/en/DestinyInventoryItemDefinition-cb4bec6f-e2b6-4f44-8593-cfd0255b89f2.json');
 
-log(fubar);
+for (var entry of definitions) {
+    if (entry.itemType===26) {
+        bountyConfigs[entry.itemHash] = struct;
+    };
+};
+
+log(bountyConfigs);
