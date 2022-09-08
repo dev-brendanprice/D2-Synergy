9245e6f2edddc1bd6b43e5c53b28e90bc47dd550
Brendan <BP7714.bp@gmail.com>
2022-09-08 17:52:19 +0100

    location href

--- www/assets/scripts/utils/ModuleScript.js
+++ www/assets/scripts/utils/ModuleScript.js
@@ -14,7 +14,7 @@ const VerifyState = async () => {
     if (state != window.localStorage.getItem('stateCode')) {
         window.localStorage.clear();
         window.sessionStorage.clear();
-        window.location.href = userStruct.homeUrl;
+        window.location.href = 'https://synergy.brendanprice.xyz/';
     }
     else {
         window.localStorage.removeItem('stateCode');
