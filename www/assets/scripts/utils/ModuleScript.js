cf0a11a1102f367c4a588fe411a9cc90e98eb2bf
Brendan <BP7714.bp@gmail.com>
2022-09-09 23:42:06 +0100

    Update ModuleScript.js

--- www/assets/scripts/utils/ModuleScript.js
+++ www/assets/scripts/utils/ModuleScript.js
@@ -14,7 +14,7 @@ const VerifyState = async () => {
     if (state != window.localStorage.getItem('stateCode')) {
         window.localStorage.clear();
         window.sessionStorage.clear();
-        window.location.href = 'http://localhost:5500/www/';
+        window.location.href = 'https://synergy.brendanprice.xyz/';
     }
     else {
         window.localStorage.removeItem('stateCode');
