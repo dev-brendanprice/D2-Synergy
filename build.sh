ef6bcdf1c118d6f6b31e7dc826c0754a44bc9951
Brendan <BP7714.bp@gmail.com>
2022-09-22 23:19:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,56 @@
-npm run build
+#!/bin/sh
+set -e
+# Install node and npm via nvm - https://github.com/creationix/nvm
+
+# Run this script like - bash script-name.sh
+
+# Define versions
+INSTALL_NODE_VER=10
+INSTALL_NVM_VER=0.33.11
+INSTALL_YARN_VER=1.7.0
+
+# You can pass argument to this script --version 8
+if [ "$1" = '--version' ]; then
+	echo "==> Using specified node version - $2"
+	INSTALL_NODE_VER=$2
+fi
+
+echo "==> Ensuring .bashrc exists and is writable"
+touch ~/.bashrc
+
+echo "==> Installing node version manager (NVM). Version $INSTALL_NVM_VER"
+# Removed if already installed
+rm -rf ~/.nvm
+# Unset exported variable
+export NVM_DIR=
+
+# Install nvm 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v$INSTALL_NVM_VER/install.sh | bash
+# Make nvm command available to terminal
+source ~/.nvm/nvm.sh
+
+echo "==> Installing node js version $INSTALL_NODE_VER"
+nvm install $INSTALL_NODE_VER
+
+echo "==> Make this version system default"
+nvm alias default $INSTALL_NODE_VER
+nvm use default
+
+echo "==> Checking for versions"
+nvm --version
+node --version
+npm --version
+yarn --version
+
+echo "==> Print binary paths"
+which npm
+which node
+which yarn
+
+echo "==> List installed node versions"
+nvm ls
+
+nvm cache clear
+echo "==> Now you're all setup and ready for development. If changes are yet totake effect, I suggest you restart your computer"
+
+# Tested on Ubuntu, MacOS
