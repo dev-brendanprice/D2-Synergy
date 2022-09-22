89965b03eb9647282790a45e36b75845c80b7abe
Brendan <BP7714.bp@gmail.com>
2022-09-22 18:44:28 +0100

    Correct build options

--- www/snowpack.config.js
+++ www/snowpack.config.js
@@ -3,10 +3,13 @@ let packageJson = require('./package.json');
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   root: 'src/',
+  buildOptions: {
+    out: './build'
+  },
   devOptions: {
     port: 5500
   },
   env: {
-    'version': `${packageJson.version}`
+    version: `${packageJson.version}`
   }
 };
