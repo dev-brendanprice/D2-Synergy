3fc6c7a6d8c57e94b86cd1e3d62677b1ad2d868d
Brendan <BP7714.bp@gmail.com>
2022-10-08 23:12:07 +0100

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,19 @@
 console.log('%cD2 SYNERGY', 'font-weight: bold;font-size: 40px;color: white;');
 console.log('// Welcome to D2Synergy, Please report any errors to @beru2003 on Twitter.');
 
+import axios from 'axios';
 
 const log = console.log.bind(console),
       localStorage = window.localStorage,
-      clientId = import.meta.env.CLIENT_ID;
+      clientId = import.meta.env.CLIENT_ID,
+      apiKey = import.meta.env.API_KEY;
+      log(apiKey);
+      log(import.meta.env.API_KEY);
 
-// Generate state parameter
-var GenerateState = (len) => {
-    let result = ' ';
-    let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    for (let i = 0; i < len; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    };
-    return result;
-};
+
 
 // Check localStorage to determine if user has signed in already
-var CheckSession = async () => {
+async function CheckSession() {
 
     var acToken = JSON.parse(localStorage.getItem('accessToken')),
         rsToken = JSON.parse(localStorage.getItem('refreshToken')),
@@ -35,8 +31,24 @@ var CheckSession = async () => {
     };
 };
 
-// Check for server availability
-fetch('https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/4289226715/', {headers: {"X-API-Key": import.meta.env.API_KEY}})
+
+// Generate state parameter
+function GenerateState(len) {
+    let result = ' ';
+    let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    for (let i = 0; i < len; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    };
+    return result;
+};
+
+
+// Main
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Check for server availability
+    log(apiKey);
+    axios.get('https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/4289226715/', {headers: {"X-API-Key": apiKey}})
     .catch((error) => {
         if (error.response) {
 
@@ -47,16 +59,13 @@ fetch('https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDef
         };
     });
 
-
-// Main
-window.addEventListener('DOMContentLoaded', () => {
-
     // Put version number in navbar
     document.getElementById('navBarVersion').innerHTML = `${import.meta.env.version}`;
 
     // Listen for authorize button click
     document.getElementById('btnAuthorize').addEventListener('click', () => {
-        var stateCode = GenerateState(128);
+        
+        const stateCode = GenerateState(128);
         localStorage.setItem('stateCode', stateCode);
         window.location.href = `https://www.bungie.net/en/oauth/authorize?&client_id=${clientId}&response_type=code&state=${stateCode}`;
     });
