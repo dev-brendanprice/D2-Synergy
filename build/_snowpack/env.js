1a7e8264b4a110baa8bbd1bf71b5c8965d3e2221
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-12-31 01:08:07 +0000

    Delete build directory

--- build/_snowpack/env.js
+++
@@ -1,8 +0,0 @@
-export const version = "Alpha v0.4.5";
-export const CLIENT_ID = "40022";
-export const API_KEY = "f7857fa32f5f4675bd49d3efb9ab3491";
-export const AUTH = "NDAwMjI6WEEwYXA0MnlTR29tM1kwLVREMFRuWDFCQjNRU1lmTXdGdjRiTTcwUGd5OA==";
-export const HOME_URL = "http://localhost:5500/";
-export const MODE = "production";
-export const NODE_ENV = "production";
-export const SSR = false;
