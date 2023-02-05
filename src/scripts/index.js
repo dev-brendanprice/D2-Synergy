eefc90ba0aed1c6691ad0507fbab8f757d438faf
Brendan <BP7714.bp@gmail.com>
2023-02-05 04:58:20 +0000

    Fix slow auth button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,15 @@
+import { MakeRequest } from './modules/MakeRequest.js';
+
 console.log('%cD2 SYNERGY', 'font-weight: bold;font-size: 40px;color: white;');
 console.log('// Welcome to D2Synergy, Please report any errors to @_devbrendan on Twitter.');
 
-import { MakeRequest } from './modules/MakeRequest.js';
+const log = console.log.bind(console);
+const localStorage = window.localStorage;
+const clientId = import.meta.env.CLIENT_ID;
+const apiKey = import.meta.env.API_KEY;
 
-const log = console.log.bind(console),
-      localStorage = window.localStorage,
-      clientId = import.meta.env.CLIENT_ID,
-      apiKey = import.meta.env.API_KEY;
+// Check for a pre-existing session
+CheckSession();
 
 
 
@@ -43,7 +46,7 @@ async function GenerateRandomString(len) {
 };
 
 
-// Main
+// Listen for DOM load
 window.addEventListener('DOMContentLoaded', function () {
 
     const stateCode = GenerateRandomString(128);
@@ -51,22 +54,14 @@ window.addEventListener('DOMContentLoaded', function () {
     // Put version number in navbar
     document.getElementById('navBarVersion').innerHTML = `${import.meta.env.version}`;
 
-    // Check for server availability, then allow auth, else do wrapper error
-    log(apiKey);
+    // Redirect user to Bungie.net on a clean slate
+    document.getElementById('btnAuthorize').addEventListener('click', () => {
+        localStorage.setItem('stateCode', stateCode);
+        window.location.href = `https://www.bungie.net/en/oauth/authorize?&client_id=${clientId}&response_type=code&state=${stateCode}&randomqueryparam=${GenerateRandomString(128)}`;
+    });
+
+    // Check for server availability, else do error (error code inside MakeRequest too)
     MakeRequest(`https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018447977370/?components=100`, {headers: {'X-API-Key': apiKey}}, {scriptOrigin: 'index', avoidCache: true})
-    .then((response) => {
-
-        // Check for a pre-existing session
-        CheckSession();
-        
-        // Listen for authorize button click
-        document.getElementById('btnAuthorize').addEventListener('click', () => {
-
-            // Redirect user to Bungie.net on a clean slate
-            localStorage.setItem('stateCode', stateCode);
-            window.location.href = `https://www.bungie.net/en/oauth/authorize?&client_id=${clientId}&response_type=code&state=${stateCode}&randomqueryparam=${GenerateRandomString(128)}`;
-        });
-    })
     .catch((error) => {
         console.error(error);
     });
