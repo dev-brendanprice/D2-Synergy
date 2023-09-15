aba7efec894dd216b5bd6ee5d1de49b3e9824e82
Brendan <BP7714.bp@gmail.com>
2023-09-15 23:07:29 +0100

    Add new landing page layout

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     syntax: 'postcss-scss',
     plugins: [
         require('@csstools/postcss-sass')({
-        includePaths: ['./node_modules']
+            includePaths: ['./node_modules']
         }),
     ]
 }
