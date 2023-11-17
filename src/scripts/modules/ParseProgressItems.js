0aa553d16d150971bc2ce6c73d0f50c319ef75a6
Brendan <BP7714.bp@gmail.com>
2023-11-17 03:28:55 +0000

    Fix challenge counter in heading

--- src/scripts/modules/ParseProgressItems.js
+++ src/scripts/modules/ParseProgressItems.js
@@ -33,6 +33,9 @@ import { CreateSeasonalChallenge } from './CreateSeasonalChallenge.js';
 
 export let seasonPassProgressionStats = {};
 
+let challengesCounter = 0;
+let completeChallengesCounter = 0;
+
 
 // Function to fetch all progressional items
 export async function ParseProgressionalItems(CharacterObjectives, CharacterInventories, characterId, characterRecords, seasonProgressionInfo, prestigeProgressionSeasonInfo, rewardsTrack, ghostModBonusXp, seasonalArtifactInfo) {
@@ -71,37 +74,7 @@ export async function ParseProgressionalItems(CharacterObjectives, CharacterInve
 
     // Get all seasonal challenges
     let currentSeasonalChallenges = await ParseSeasonalChallenges(UserProfile.currentSeasonHash, seasonProgressionInfo);
-    let totalSeasonalChallenges = 0;
     returnObj.challenges = {};
-
-    // Separate challenges based on their completion status
-    let completedChallenges = {};
-    let notCompletedChallenges = {};
-    let allSeasonalChallengesAndTheirDivs = {};
-    let completedChallengesCount = 0;
-    let notCompletedChallengesCount = 0;
-
-    // Add objectives to (all) seasonal challenges
-    // for (const recordHash in characterRecords) {
-
-    //     const objectives = characterRecords[recordHash].objectives;
-    //     if (objectives && objectives.length > 0) {
-
-    //         if (objectives.every((objective) => objective.complete)) {
-    //             completedChallenges[recordHash] = {};
-    //             completedChallenges[recordHash].displayProperties = recordDefinitions[recordHash].displayProperties;
-    //             completedChallenges[recordHash].objectives = objectives;
-    //         }
-    //         else {
-    //             notCompletedChallenges[recordHash] = {};
-    //             notCompletedChallenges[recordHash].displayProperties = recordDefinitions[recordHash].displayProperties;
-    //             notCompletedChallenges[recordHash].objectives = objectives;
-    //         };
-    //     };
-    // };
-
-    let challengesCounter = 0;
-    let completeChallengesCounter = 0;
 
     // Build group containers and seasonal challenges UI section
     let groupNames = []; log(currentSeasonalChallenges);
@@ -203,262 +176,11 @@ export async function ParseProgressionalItems(CharacterObjectives, CharacterInve
             
         };
 
-        for (let challenge of weekData.challenges) {
+        log(challengesCounter, completeChallengesCounter);
+        // Push HTML fields for challenges header stats
+        document.getElementById('challengesTotalField').innerHTML = `${challengesCounter}`;
+        document.getElementById('challengesCompletedField').innerHTML = `${completeChallengesCounter}`;
 
-            // log(challenge);
-
-            // let challengeHash = challenge.hash;
-
-            // // Create HTML element for challenge
-            // let challengeContainer = document.createElement('div');
-            // let challengeHeadingContainer = document.createElement('div');
-            // let challengeHeadingAttributeContainer = document.createElement('div');
-            // let challengeIcon = document.createElement('img');
-            // let challengeName = document.createElement('div');
-            // let challengeDescription = document.createElement('div');
-            // let challengeProgressContainer = document.createElement('div');
-            // let challengeProgressTrack = document.createElement('div');
-            // let challengeProgressPercentBar = document.createElement('div');
-            // let challengeRewardsContainer = document.createElement('div');
-            // let challengeProgressAttributesContainer = document.createElement('div');
-
-            // // Set attributes for challenge container
-            // challengeContainer.className = 'challengeContainer';
-            // challengeHeadingContainer.className = 'challengeHeadingContainer';
-            // challengeHeadingAttributeContainer.className = 'challengeHeadingAttributeContainer';
-            // challengeContainer.id = `${challengeHash}`;
-            // challengeIcon.className = 'challengeIcon';
-            // challengeName.className = 'challengeName';
-            // challengeDescription.className = 'challengeDescription';
-            // challengeProgressContainer.className = 'challengeProgressContainer';
-            // challengeProgressTrack.className = 'challengeProgressTrack';
-            // challengeProgressPercentBar.className = 'challengeProgressPercentBar';
-            // challengeRewardsContainer.className = 'challengeRewardsContainer';
-            // challengeProgressAttributesContainer.className = 'challengeProgressAttributesContainer';
-
-            // // Set attributes for content
-            // challengeDescription.innerHTML = challenge.displayProperties.description;
-            // challengeName.innerHTML = challenge.displayProperties.name;
-            // challengeIcon.src = `https://www.bungie.net${challenge.displayProperties.icon}`;
-            // challengeContainer.style.userSelect = 'none';
-
-            // // Check if challenge is completed
-            // if (completedChallenges[challengeHash]) {
-
-            //     challengeContainer.classList.add('challengeContainerComplete');
-            //     challenge.isComplete = true;
-
-            //     // Check if challenge has been claimed
-            //     let destinyProfileRecords = UserProfile.destinyUserProfile.characterRecords.data[characterId].records;
-            //     if (destinyProfileRecords[challengeHash]) {
-
-            //         if (destinyProfileRecords[challengeHash].state === 0) {
-            //             challenge.isClaimed = false;
-            //             challengeContainer.classList.remove('challengeContainerComplete');
-            //             challengeContainer.classList.add('challengeContainerCompletedNotClaimed');
-            //         }
-            //         else if (destinyProfileRecords[challengeHash].state === 1) {
-            //             challenge.isClaimed = true;
-            //         };
-            //     };
-
-            // }
-            // else {
-            //     challengeContainer.classList.add('challengeContainerNotComplete');
-            //     challenge.isComplete = false;
-            // };
-
-            // // Append all the content together
-            // challengeHeadingContainer.append(challengeIcon, challengeHeadingAttributeContainer);
-            // challengeHeadingAttributeContainer.append(challengeName, challengeDescription);
-            // challengeContainer.appendChild(challengeHeadingContainer);
-
-            // // Store the challenge and its div
-            // allSeasonalChallengesAndTheirDivs[challengeHash] = {};
-            // allSeasonalChallengesAndTheirDivs[challengeHash].container = challengeContainer;
-            // allSeasonalChallengesAndTheirDivs[challengeHash].challenge = challenge;
-
-            // // Append objectives to the challenge
-            // allSeasonalChallengesAndTheirDivs[challengeHash].challenge.objectives = [];
-            // if (notCompletedChallenges[challengeHash] || completedChallenges[challengeHash]) {
-
-            //     let challengeObjectives;
-
-            //     // Parse non-completed objectives
-            //     if (Object.keys(notCompletedChallenges).includes(challengeHash)) {
-
-            //         challengeObjectives = notCompletedChallenges[challengeHash].objectives;
-            //         for (let objective in challengeObjectives) {
-            //             allSeasonalChallengesAndTheirDivs[challengeHash].challenge.objectives.push(notCompletedChallenges[challengeHash].objectives[objective]);
-            //         };
-            //     };
-
-            //     // Parse completed objectives
-            //     if (Object.keys(completedChallenges).includes(challengeHash)) {
-
-            //         challengeObjectives = completedChallenges[challengeHash].objectives;
-            //         for (let objective in challengeObjectives) {
-            //             allSeasonalChallengesAndTheirDivs[challengeHash].challenge.objectives.push(completedChallenges[challengeHash].objectives[objective]);
-            //         };
-            //     };
-            // };
-
-            // // Add objective element to challenge
-            // for (let objective of challenge.objectives) {
-
-            //     // Objective container
-            //     let objectiveContainer = document.createElement('div');
-
-            //     // Checkbox
-            //     let objectiveCheckboxOuter = document.createElement('div');
-            //     let objectiveCheckboxMiddle = document.createElement('div');
-            //     let objectiveCheckboxInner = document.createElement('div');
-
-            //     // Checkbox style
-            //     objectiveCheckboxOuter.className = 'objectiveCheckboxOuter';
-            //     objectiveCheckboxMiddle.className = 'objectiveCheckboxMiddle';
-            //     objectiveCheckboxInner.className = 'objectiveCheckboxInner';
-
-            //     // Progress bar (objectiveAttributes doubles as a container)
-            //     let objectiveAttributes = document.createElement('div');
-            //     let objectiveName = document.createElement('div');
-            //     let objectiveProgress = document.createElement('div');
-
-            //     // Progress bar style
-            //     objectiveAttributes.className = 'objectiveAttributes';
-            //     objectiveContainer.className = 'objectiveContainer';
-            //     objectiveName.className = 'objectiveName';
-            //     objectiveProgress.className = 'objectiveProgress';
-
-            //     // InnerHTML values etc
-            //     objectiveName.innerHTML = objectiveDefinitions[objective.objectiveHash].progressDescription || 'Completed';
-            //     objectiveProgress.innerHTML = `${objective.progress}/${objective.completionValue}`;
-
-            //     // If objective is complete
-            //     if (objective.complete) {
-
-            //         // Change innerHTML to avoid overflow values
-            //         objectiveProgress.innerHTML = `${objective.completionValue}/${objective.completionValue}`;
-
-            //         // Change checkbox style
-            //         objectiveCheckboxOuter.style.borderColor = 'var(--challengeCompletedCheckboxOuter)';
-            //         objectiveCheckboxMiddle.style.borderColor = 'var(--challengeCompletedCheckboxMiddle)';
-            //         objectiveCheckboxInner.style.backgroundColor = 'var(--challengeCompletedCheckboxInner)';
-                    
-            //         // Change attribute container style (to children too)
-            //         objectiveName.style.color = '#BABABA';
-            //         objectiveProgress.style.color = '#BABABA';
-            //         objectiveAttributes.style.color = '#494949';
-            //     };
-
-            //     // Append elements to their parents
-            //     objectiveCheckboxOuter.appendChild(objectiveCheckboxMiddle);
-            //     objectiveCheckboxMiddle.appendChild(objectiveCheckboxInner);
-            //     objectiveAttributes.append(objectiveName, objectiveProgress);
-
-            //     // Push elements to top-most parent
-            //     objectiveContainer.append(objectiveCheckboxOuter, objectiveAttributes);
-
-            //     // Push objectives to parent container
-            //     challengeProgressAttributesContainer.appendChild(objectiveContainer);
-
-            // };
-            // challengeContainer.appendChild(challengeProgressAttributesContainer);
-
-            // // Add reward items to the challenge
-            // if (challenge.rewardItems) {
-
-            //     // Loop over challenge reward items
-            //     let challengeRewardItems = challenge.rewardItems;
-            //     for (let i=0; i<challengeRewardItems.length; i++) {
-
-            //         // root reward from current iteration
-            //         let reward = challengeRewardItems[i];
-                    
-            //         // Create DOM elements
-            //         let rewardContainer = document.createElement('div');
-            //         let rewardName = document.createElement('div');
-            //         let rewardIcon = document.createElement('img');
-
-            //         // Get corresponding reward values, based on the reward name
-            //         let rewardUiName = itemDefinitions[reward.itemHash].displayProperties.name;
-            //         let previousRewardUiName;
-
-            //         // Check the next reward is bright dust, if so go to the previous iteration to get the corresponding reward value
-            //         if (challengeRewardItems[i-1]) {
-
-            //             previousRewardUiName = itemDefinitions[challengeRewardItems[i-1].itemHash].displayProperties.name;
-            //             let currentRewardName = itemDefinitions[challengeRewardItems[i].itemHash].displayProperties.name;
-
-            //             // Bright dust reward value
-            //             if (previousRewardUiName.includes('Challenger XP')) {
-            //                 let rewardValue = InsertSeperators(rewardsBasedOnChallengerXP[previousRewardUiName]);
-            //                 rewardName.innerHTML = `${currentRewardName} (${rewardValue})`;
-            //             };
-
-            //             // Singular challenger XP reward value
-            //             if (currentRewardName.includes('Challenger XP')) {
-            //                 let rewardValue = InsertSeperators(rewardsBasedOnSingularItems[currentRewardName]);
-            //                 rewardName.innerHTML = `${currentRewardName} (${rewardValue})`;
-            //             };
-            //         }
-
-            //         // Else, use reward values based on singular items
-            //         else {
-            //             let rewardValue = InsertSeperators(rewardsBasedOnSingularItems[rewardUiName]);
-            //             rewardName.innerHTML = `${rewardUiName} (${rewardValue})`;
-
-            //             // ..
-            //             if (rewardUiName.includes('Challenger XP')) {
-            //                 // log(rewardUiName, rewardValue);
-            //             };
-            //         };
-
-            //         rewardContainer.className = 'singleChallengeRewardContainer';
-            //         rewardIcon.src = `https://www.bungie.net${itemDefinitions[reward.itemHash].displayProperties.icon}`;
-            //         rewardName.className = 'challengeRewardName';
-            //         rewardIcon.className = 'challengeRewardIcon';
-
-            //         rewardContainer.append(rewardIcon, rewardName);
-            //         challengeRewardsContainer.appendChild(rewardContainer);
-            //     };
-
-            //     challengeContainer.appendChild(challengeRewardsContainer);
-            // };
-
-            // // Check if the challenge is completed, set isComplete to true in guard statement, otherwise false by default
-            // // This is to make it easier to check if the challenge is complete, as opposed to comparing with completedChallenges
-            // if (completedChallenges[challengeHash]) {
-            //     completedChallengesCount++;
-            //     allSeasonalChallengesAndTheirDivs[challengeHash].challenge.isComplete = true;
-            // }
-            // else {
-            //     notCompletedChallengesCount++;
-            //     allSeasonalChallengesAndTheirDivs[challengeHash].challenge.isComplete = false;
-            // };
-
-            // // Sort challenge completion progress as a percentage of the total completion value
-            // let challengeObjectiveProgressTotal = 0;
-            // let challengeObjectiveCompletionTotal = 0;
-            // let challengeObjectives = allSeasonalChallengesAndTheirDivs[challengeHash].challenge.objectives;
-
-            // for (const objective in challengeObjectives) {
-            //     challengeObjectiveProgressTotal += challengeObjectives[objective].progress;
-            //     challengeObjectiveCompletionTotal += challengeObjectives[objective].completionValue;
-            // };
-
-            // // Calculate progress as a percentage, if objective is "0/1" then it is a boolean,
-            // // so set progress (if not complete) or 100% (if complete, avoid overflow)
-            // allSeasonalChallengesAndTheirDivs[challengeHash].challenge.completionPercentage = (challengeObjectiveProgressTotal / challengeObjectiveCompletionTotal) * 100;
-
-            // // Change width of challengeProgressPercentBar based on completion percentage
-            // if (allSeasonalChallengesAndTheirDivs[challengeHash].challenge.completionPercentage >= 100) {
-            //     challengeProgressPercentBar.style.width = '100%';
-            // }
-            // else {
-            //     challengeProgressPercentBar.style.width = `${allSeasonalChallengesAndTheirDivs[challengeHash].challenge.completionPercentage}%`;
-            // };
-        };
     };
 
 
@@ -511,9 +233,6 @@ export async function ParseProgressionalItems(CharacterObjectives, CharacterInve
     //     document.getElementById('seasonalChallengeItems').appendChild(chunkContainer);
     // };
 
-    // Push HTML fields for challenges header stats
-    document.getElementById('challengesTotalField').innerHTML = challengesCounter;
-    document.getElementById('challengesCompletedField').innerHTML = completeChallengesCounter;
     // [ -- END OF SEASONAL CHALLENGES -- ]
 
 
